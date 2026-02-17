375a1fa34361805faa4d82881a841389ef6c2187
Olga Barovskaya <asdfg1234_08@mail.ru>
2026-02-17 21:21:39 +0300

    feat: attempt

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import AppRouter from "./routes/AppRouter";
 import * as Layout from "@/layouts";
 
 export function App() {
+  console.log("launched");
   return (
     <Layout.Wrapper>
       <AppRouter />
